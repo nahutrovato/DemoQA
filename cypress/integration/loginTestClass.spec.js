7607c360e50dcb331f7a200d74d7646e31552b16
nahutrovato <trovatonahuel@gmail.com>
2022-04-30 20:38:29 -0300

    Create test label text

--- cypress/integration/loginTestClass.spec.js
+++ cypress/integration/loginTestClass.spec.js
@@ -3,12 +3,19 @@ import loginPage from "../pages/loginPage";
 describe('Login test with class',() => {
 
     before('Enter to the page',() =>{
-        cy.visit('https://demoqa.com/login');
+        cy.visit('/login');
+    })
+
+    it('Check label texts',() => {
+        loginPage.userNameLabel('UserName :');
+        loginPage.passwordLabel('Password :');
     })
     
-    it('Login test',() => {
+    it('Login test with wrong credentials',() => {
         loginPage.typeUserName('Nahuel test');
         loginPage.typepassword('test');
         loginPage.clickLoginBtn();
+        cy.wait(2000);
+       
     })
 })
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,9 +1,19 @@
 class loginPage{
 
     elements = {
+        userNameLabel: () => cy.get('#userName-label'),
+        passwordLabel: () => cy.get('#password-label'),
         usernameInput: () => cy.get('#userName'),
         passwordInput: () => cy.get('#password'),
         loginBtn: () => cy.get('#login')
+    }
+
+    userNameLabel(text){
+        this.elements.userNameLabel().contains(text);
+    }
+
+    passwordLabel(text){
+        this.elements.passwordLabel().contains(text);
     }
 
     typeUserName(username){
